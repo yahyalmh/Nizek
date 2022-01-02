4da34e41e900e49b01cbf14245757e1b32f89219
yahyalmh <yahyamh01@gmail.com>
2022-01-02 12:57:17 +0330

    Add Application calss to init user config at first

---
+++ app/src/main/java/com/nizek/challenge/ApplicationLoader.kt
@@ -0,0 +1,15 @@
+package com.nizek.challenge
+
+import android.app.Application
+
+/**
+ * @author yaya (@yahyalmh)
+ * @since 31th December 2021
+ */
+
+class ApplicationLoader : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        UserConfig.getInstance(applicationContext)
+    }
+}
