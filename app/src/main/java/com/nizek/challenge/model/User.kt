b8b778ac417be96d68c1d1552ede2a6c0a91e060
yahyalmh <yahyamh01@gmail.com>
2022-01-02 12:35:26 +0330

    Add user model class

---
+++ app/src/main/java/com/nizek/challenge/model/User.kt
@@ -0,0 +1,10 @@
+package com.nizek.challenge.model
+
+/**
+ * @author yaya (@yahyalmh)
+ * @since 31th December 2021
+ */
+
+data class User(val fullName: String?, val username: String?, val password: String?) {
+
+}
