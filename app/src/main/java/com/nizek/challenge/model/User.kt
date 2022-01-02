cb9e409f5cef993baa876f1253cbf33853d37034
yahyalmh <yahyamh01@gmail.com>
2022-01-02 12:46:43 +0330

    remove unncessary block

--- app/src/main/java/com/nizek/challenge/model/User.kt
+++ app/src/main/java/com/nizek/challenge/model/User.kt
@@ -5,6 +5,4 @@ package com.nizek.challenge.model
  * @since 31th December 2021
  */
 
-data class User(val fullName: String?, val username: String?, val password: String?) {
-
-}
+data class User(val fullName: String?, val username: String?, val password: String?) 
