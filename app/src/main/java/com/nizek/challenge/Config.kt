f02a85f6cff0c767a319013bbafe8b4602490e2b
yahyalmh <yahyamh01@gmail.com>
2022-01-02 12:52:11 +0330

    Add config class to save config

---
+++ app/src/main/java/com/nizek/challenge/Config.kt
@@ -0,0 +1,16 @@
+package com.nizek.challenge
+
+/**
+ * @author yaya (@yahyalmh)
+ * @since 31th December 2021
+ */
+
+class Config {
+    companion object {
+        const val SHARED_PREF_NAME = "main_pref"
+        const val FULL_NAME_PREF_KEY = "full_name"
+        const val USERNAME_PREF_KEY = "username"
+        const val PASSWORD_PREF_KEY = "password"
+        const val FULL_NAME_ARGUMENT_KEY = "fullName"
+    }
+}
